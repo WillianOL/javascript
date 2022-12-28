f91014bfbfeae435354c7a14cd616fca2da1e48f
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2022-12-28 10:24:09 -0300

    Create ex014.js

---
+++ aulas/aula13/ex014.js
@@ -0,0 +1,22 @@
+let contador = 1
+
+
+//===Estrutura de repetição com teste lógico no final===
+do{
+    console.log(`Passo ${contador}`)
+    contador++
+} while(contador <= 20)
+
+/*
+===Estrutura de repetição com teste lógico no inicio===
+
+//Enquanto a vareavel contador for menor ou igual a 200, adicionar mais um a ela. Até a condição ser falsa
+while( contador <= 200 ){
+    console.log(`passo ${contador}`);
+    contador++ //Adiconando mais 1
+}
+*/
+
+
+
+
