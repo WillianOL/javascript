cd1e037e058c68e7847207a91b6c430f76b8b1af
Will Oliveira <112639055+WillianOL@users.noreply.github.com>
2023-01-04 14:19:31 -0300

    Atualizações

--- aulas/aula16/ex018.js
+++ aulas/aula16/ex018.js
@@ -1,11 +1,9 @@
-function ParImp(n){
-    if(n%2 == 0){
-        return 'Par!'
-    } else{
-        return 'Impar!'
+function CalNota(nota){ //Função para verificar nota, com o parâmetro 'nota'
+    if(nota < 6){ //Se a nota for menor que 6, retornar REPROVADO para a chamada
+        return 'REPROVADO'
+    }else if(nota >= 6 && nota < 10){
+        return 'APROVADO' //Se ela for maior que 6 e menor que 10, retortar APROVADO para a chamada
     }
 }
 
-let resultado = ParImp(5)
-
-console.log(resultado)
+console.log(CalNota(10)) //Chamada para a ação acontecer.
---
+++ aulas/aula16/ex019.js
@@ -0,0 +1,7 @@
+function Dividir( n1, n2){
+    return n1 / n2
+}
+
+let res = Dividir(10, 5);
+
+console.log(res)
